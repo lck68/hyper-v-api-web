8eb3e6e85548289741c6c858eb25fa9299404588
LCK <2276842367@qq.com>
2023-05-07 17:22:51 +0800

    Update 仅写bin.py

--- 仅写bin.py
+++ 仅写bin.py
@@ -1,5 +1,5 @@
 import random
 
 with open("index.bin", "ab") as f:
-        random_data = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=5))
-        f.write(random_data)
+        aa = random.randint(10,99)
+        f.write(aa)
